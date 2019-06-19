9b5bc5cdfe7cec59e4999dd8fea5101973a04977
Yuvraj Sharma <yuvraj_2300@hotmail.com>
2019-06-19 17:13:03 +0530

    Update

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,8 @@ package io.javabrains.moviecatalogservice.resources;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +32,7 @@ public class MovieCatalogResource {
 		
 		
 		//hardcoded list
-		return Collections.singletonList
-				(new CatalogItem("Once upon a time in Hollywood", "Brad Pitt !!!", 4));
-	}
+		return ratings.stream().map(rating->new	CatalogItem("avc","test",4))
+				.collect(Collectors.toList());
+			}
 }
