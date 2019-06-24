aad67b02b04b5398ad620ce6288d8ba43a2c07c3
yuvraj <yuvraj_2300@hotmail.com>
2019-06-24 23:45:52 +0530

    Added RestTemplate

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,13 +2,19 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+	@Bean
+	public RestTemplate getRestTemp() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
 
 }
-
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,38 +1,42 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
-	
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@GetMapping("/{userid}")
 	public List<CatalogItem> listOfCatalog(@PathVariable("userid") String userid) {
-		
-		//get all rated movie IDs
-		List<Rating>	ratings	=	Arrays.asList(
-				new	Rating("123",4),
-				new	Rating("456",3)
-			);
-		//for each movie ID, call movie info service to get the details\
-		
-		//Put them all together.
-		
-		
-		//hardcoded list
-		return ratings.stream().map(rating->new	CatalogItem("avc","test",4))
-				.collect(Collectors.toList());
-			}
+
+		restTemplate = new RestTemplate();
+		// calling other service
+
+		// get all rated movie IDs
+		List<Rating> ratingsList = Arrays.asList(new Rating("123", 4), new Rating("456", 3));
+		// for each movie ID, call movie info service to get the details\
+
+		// Put them all together.
+
+		// hardcoded list
+		return ratingsList.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Description", rating.getRating());
+		}).collect(Collectors.toList());
+	}
 }
